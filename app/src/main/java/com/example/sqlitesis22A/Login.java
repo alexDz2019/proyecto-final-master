420d0841d31628607fdc4a00e79b992ab52af6ff
alexDz2019 <jonathanmoreno.jm49@gmail.com>
2019-11-05 15:02:16 -0600

    agregue el login

---
+++ app/src/main/java/com/example/sqlitesis22A/Login.java
@@ -0,0 +1,15 @@
+package com.example.sqlitesis22A;
+
+import android.os.Bundle;
+
+import com.example.crudmysqlandroid.R;
+
+public class Login {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
